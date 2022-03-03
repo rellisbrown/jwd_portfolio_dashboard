38305db2aedc918e1cd5cea1612f95c39c366e70
rellisbrown <rellisbrown@calibrateconsulting.com>
2022-03-03 15:03:05 +0000

    fixing issues etc

--- src/components/lineChart/LineChart.jsx
+++ src/components/lineChart/LineChart.jsx
@@ -1,29 +1,31 @@
 import React, { useEffect, useRef, useContext } from 'react';
 import * as d3 from 'd3';
-import { axisLeft, axisTop } from 'd3';
+import { axisLeft, axisTop, axisBottom } from 'd3';
 import { DataContext } from '../../utils/DataContext';
+import useResizeObserver from '../../utils/useResizeObserver';
 
 export default function LineChart() {
   const contextValue = useContext(DataContext);
   const svgRef = useRef(null);
+  const wrapperRef = useRef();
+  const dimensions = useResizeObserver(wrapperRef);
 
-  console.log(contextValue.portfolioDetails);
-  const innerHeight = 300;
-  const innerWidth = 550;
-  const margin = { right: 150, bottom: 150, top: 50, left: 150 };
-  const outerHeight = innerHeight + margin.top + margin.bottom;
-  const outerWidth = innerWidth + margin.left + margin.right;
+  const margin = { right: 50, bottom: 100, top: 50, left: 100 };
+  const outerHeight = dimensions?.height;
+  const outerWidth = dimensions?.width;
+  const innerHeight = outerHeight - margin.top - margin.bottom;
+  const innerWidth = outerWidth - margin.left - margin.right;
 
   const lineData = contextValue.portfolioDetails[0].data.map((item) => ({
     date: item.date,
     value: item.close,
   }));
-  console.log(contextValue.portfolioDetails);
+
   const yValues = lineData.map((item) => item.value);
   const yScale = d3
     .scaleLinear()
-    .domain([d3.min(yValues), d3.max(yValues)])
-    .range([0, outerWidth]);
+    .domain([d3.min(yValues) - 10, d3.max(yValues)])
+    .range([innerHeight, 0]);
 
   const xValues = lineData.map((item) => item.date);
   const xScale = d3
@@ -31,18 +33,27 @@ export default function LineChart() {
     .domain([d3.min(xValues), d3.max(xValues)])
     .range([0, innerWidth]);
 
-  const xAxis = axisTop(xScale)
+  const xAxis = axisBottom(xScale)
     .tickFormat((i) => i.getFullYear())
     .tickSizeOuter(10)
     .tickPadding(5);
 
-  const yAxis = axisLeft(yScale).tickFormat((i) => `${i}%`);
+  const yAxis = axisLeft(yScale).tickFormat((i) => `$${i}`);
 
   useEffect(() => {
+    d3.select(svgRef.current).select('.x-axis').remove();
+    d3.select(svgRef.current).select('.y-axis').remove();
+    d3.select(svgRef.current).select('.title').remove();
+    d3.select(svgRef.current).selectAll('g').remove();
+    d3.select(svgRef.current).select('.line').remove();
+    d3.select(svgRef.current).select('.x-label').remove();
+    d3.select(svgRef.current).select('.y-label').remove();
+
     d3.select(svgRef.current)
       .append('g')
       .call(yAxis)
-      .attr('transform', `translate(${margin.left}, ${margin.top})`);
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
+      .attr('class', 'y-axis');
 
     d3.select(svgRef.current)
       .append('g')
@@ -50,7 +61,8 @@ export default function LineChart() {
       .attr(
         'transform',
         `translate(${margin.left},${innerHeight + margin.top})`
-      );
+      )
+      .attr('class', 'x-axis');
 
     d3.select(svgRef.current)
       .append('g')
@@ -60,7 +72,7 @@ export default function LineChart() {
       .append('circle')
       .attr('cx', (d) => xScale(d.date))
       .attr('cy', (d) => yScale(d.value))
-      .attr('r', 5)
+      .attr('r', 2)
       .attr('transform', `translate(${margin.left}, ${margin.top})`)
       .style('fill', 'pink');
 
@@ -79,7 +91,7 @@ export default function LineChart() {
 
       .style('fill', 'none')
       .style('stroke', 'pink')
-      .style('stroke-width', '5');
+      .style('stroke-width', '2');
 
     d3.select(svgRef.current)
       .append('text')
@@ -88,25 +100,28 @@ export default function LineChart() {
       .attr('text-anchor', 'start')
       .attr('font-size', '2.00rem')
       .attr('font-weight', '1000')
-      .text('stock');
+      .attr('class', 'title')
+      .text('Stock price over time');
 
     d3.select(svgRef.current)
       .append('text')
-      .attr('x', innerWidth / 2 + 50)
+      .attr('x', innerWidth / 2 + margin.left)
       .attr('y', innerHeight + 90)
       .attr('text-anchor', 'start')
       .attr('font-size', '1rem')
       .attr('font-weight', '400')
-      .text('year');
+      .attr('class', 'y-label')
+      .text('Date');
 
     d3.select(svgRef.current)
       .append('text')
       .attr('x', 21)
-      .attr('y', innerHeight / 2)
+      .attr('y', innerHeight / 2 + margin.top)
       .attr('text-anchor', 'start')
       .attr('font-size', '1.2rem')
       .attr('font-weight', '200')
-      .text('stock ');
+      .attr('class', 'x-label')
+      .text('Price');
   }, [
     lineData,
     margin.top,
@@ -116,10 +131,14 @@ export default function LineChart() {
     yAxis,
     xScale,
     yScale,
+    innerHeight,
+    innerWidth,
   ]);
 
+  if (!dimensions) return <div ref={wrapperRef}>loading...</div>;
+
   return (
-    <div>
+    <div style={{ width: '100%', height: '500px' }} ref={wrapperRef}>
       <svg width={outerWidth} height={outerHeight} ref={svgRef} />
     </div>
   );
